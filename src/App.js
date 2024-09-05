9b773dca9a51cfe71abe13c5fc44034421c70aa6
patelshivam <shivampatel3650@gmail.com>
2024-09-05 10:28:42 +0530

    signup is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Signup from "./components/Signup/Signup";
 
 function App() {
+  // State to track if the user is logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle login (this could be passed to the Signup component)
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-      <Navbar />
+      {!isLoggedIn && <Signup onLogin={handleLogin} />}
+      {isLoggedIn && <Navbar />}
     </div>
   );
 }
